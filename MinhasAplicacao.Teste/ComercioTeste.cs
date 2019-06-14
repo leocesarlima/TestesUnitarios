b08a78401a63a1c06fc1cd22e253591365fb8f3d
Alex Alves <alex.silva@bhs.com.br>
2019-06-14 08:54:23 -0300

    criação de novo projeto de testes

---
+++ MinhasAplicacao.Teste/ComercioTeste.cs
@@ -0,0 +1,50 @@
+﻿using MinhaAplicacao.Dominios;
+using MinhaAplicacao.Modelos;
+using MinhaAplicacao.Servicos;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MinhasAplicacao.Teste
+{
+    public class ComercioTeste
+    {
+        private readonly Mock<IUsuarioServico> _usuarioServico;
+        private readonly ComercioDominio comercioDominio;
+
+        public ComercioTeste()
+        {
+            _usuarioServico = new Mock<IUsuarioServico>();
+            comercioDominio = new ComercioDominio(_usuarioServico.Object);
+        }
+
+        [Fact]
+        public void Comercio_ListarUmCliente_Sucesso()
+        {
+            bool flag = false;
+
+            _usuarioServico
+                .Setup(s => s.RecuperarTodos())
+                .Returns(async () => new List<UsuarioModelo>
+                {
+                    new UsuarioModelo
+                    {
+                        Id = 1
+                    }
+                })
+                .Callback(() => flag = true);
+
+            var resultado = comercioDominio.ListarClientes();
+
+            Assert.True(flag);
+        }
+
+        [Fact]
+        public void Comercio_CalcularLucro_Sucesso()
+        {
+            var resultado = comercioDominio.CalcularLucro(10).ToString("N1");
+
+            Assert.Equal("11,0", resultado);
+        }
+    }
+}
